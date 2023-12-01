c00c39f20d4cef45fa6441f10669a5541502e150
Escbrk <69437294+Escbrk@users.noreply.github.com>
2023-11-30 19:05:49 -0700

    hw-1 is DONE

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,3 +3,29 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+};
+refs.stop.disabled = true;
+
+refs.start.addEventListener('click', onStart);
+refs.stop.addEventListener('click', onStop);
+
+function onStart() {
+  refs.start.disabled = true;
+  refs.stop.disabled = false;
+
+  interval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+
+function onStop() {
+  refs.start.disabled = false;
+  refs.stop.disabled = true;
+
+  clearInterval(interval);
+}
