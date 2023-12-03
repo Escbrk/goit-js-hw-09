d78658f8cec6aab4a468ab3f1aa1a9a1ee1934e9
Paul <69437294+Escbrk@users.noreply.github.com>
2023-12-02 17:19:59 -0700

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,12 +1,4 @@
-function createPromise(position, delay) {
-  
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+import { Notify } from 'notiflix';
 
 const refs = {
   form: document.querySelector('.form'),
@@ -15,3 +7,45 @@ const refs = {
   amount: document.querySelector('[name="amount"]'),
   submitBtn: document.querySelector('[type="submit"]'),
 };
+
+let intervalId;
+
+function createPromise(position, delay) {
+  const obj = { position, delay };
+  const shouldResolve = Math.random() > 0.3;
+
+  return new Promise((res, rej) => {
+    if (shouldResolve) {
+      // Fulfill
+      res(obj);
+    } else {
+      // Reject
+      rej(obj);
+    }
+  });
+}
+
+refs.submitBtn.addEventListener('click', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  const delayToNum = Number(refs.delay.value);
+  const stepToNum = Number(refs.step.value);
+  let totalMs;
+  const toNum = Number(refs.amount.value);
+
+  for (let i = 0; i < toNum; i += 1) {
+    totalMs = delayToNum + stepToNum;
+    clearInterval(intervalId);
+  }
+
+  intervalId = setInterval(() => {
+    createPromise(delayToNum, stepToNum)
+      .then(({ position, delay }) => {
+        Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
+  }, delayToNum);
+}
