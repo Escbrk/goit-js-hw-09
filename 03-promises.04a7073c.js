9b3160392bcb044db6d9d6a0a7e4dea927ef8471
Escbrk <69437294+Escbrk@users.noreply.github.com>
2023-12-03 00:20:34 +0000

    Deploying to gh-pages from @ Escbrk/goit-js-hw-09@d78658f8cec6aab4a468ab3f1aa1a9a1ee1934e9 🚀

--- 03-promises.04a7073c.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".form"),document.querySelector('[name="delay"]'),document.querySelector('[name="step"]'),document.querySelector('[name="amount"]'),document.querySelector('[type="submit"]');
-//# sourceMappingURL=03-promises.04a7073c.js.map
